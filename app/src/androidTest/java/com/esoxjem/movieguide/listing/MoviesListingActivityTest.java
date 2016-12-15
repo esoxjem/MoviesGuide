5754afc5cc5a724a5d9ab3c3d5f7b1ee911e5d1d
Arun Sasidharan <arunsasidharan121@gmail.com>
2016-12-15 15:41:21 +0530

    Updated MoviesListingActivityTest

--- app/src/androidTest/java/com/esoxjem/movieguide/listing/MoviesListingActivityTest.java
+++ app/src/androidTest/java/com/esoxjem/movieguide/listing/MoviesListingActivityTest.java
@@ -1,5 +1,6 @@
 package com.esoxjem.movieguide.listing;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -10,9 +11,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * @author arunsasidharan
@@ -35,5 +38,13 @@ public class MoviesListingActivityTest
     {
         Thread.sleep(3000);
         onView(withId(R.id.movies_listing)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void shouldBeAbleToScrollViewMovieDetails() throws InterruptedException
+    {
+        Thread.sleep(3000);
+        onView(withId(R.id.movies_listing)).perform(RecyclerViewActions.actionOnItemAtPosition(10, click()));
+        onView(withText("Summary")).check(matches(isDisplayed()));
     }
 }
